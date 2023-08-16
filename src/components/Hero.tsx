9a336db28f9f19d9ad61701447a15790d1c0e537
nazimboudeffa <boudeffa@gmail.com>
2023-08-16 17:19:20 +0200

    Initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,41 @@
+function Hero () {
+    return (
+        <section
+        className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center"
+        >
+        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+            <div
+            className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right"
+            >
+            <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
+                The best way to manage your online business
+            </h2>
+
+            <p className="hidden text-gray-500 md:mt-4 md:block">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
+                tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et
+                fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt
+                duis.
+            </p>
+
+            <div className="mt-4 md:mt-8">
+                <a
+                href="#"
+                className="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                >
+                Get Started Today
+                </a>
+            </div>
+            </div>
+        </div>
+
+        <img
+            alt="Violin"
+            src="https://images.unsplash.com/photo-1665686310934-8fab52b3821b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            className="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
+        />
+        </section>
+    )
+}
+
+export default Hero
