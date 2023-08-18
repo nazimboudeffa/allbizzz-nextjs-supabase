741fe8055c9d1fc04214c3aadc5d2a4498d6c94a
nazimboudeffa <boudeffa@gmail.com>
2023-08-18 17:26:22 +0200

    Fixing Auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,6 +3,7 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import Link from 'next/link'
 
 import type { Database } from '@/lib/database.types'
 
@@ -31,6 +32,15 @@ export default function Login() {
     router.refresh()
   }
 
+  const handleSignInWithGoogle = async() => {
+    await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo: `${location.origin}/auth/callback`,
+                  },
+    })
+}
+
   const handleSignOut = async () => {
     await supabase.auth.signOut()
     router.refresh()
@@ -38,16 +48,73 @@ export default function Login() {
 
   return (
     <>
-      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+
+<form 
+  onSubmit={handleSignIn}
+  className="flex flex-col w-full justify-center items-center mt-8 space-y-5"
+>
+  <label 
+      htmlFor="email" 
+      className="block test-sm font-medium test-gray-700"
+  >
+      Email
+  </label>
+  <div>
       <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
+          type="email"
+          name="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="username@example.com"
+          onChange = {(e) => setEmail(e.target.value)}
       />
-      <button onClick={handleSignUp}>Sign up</button>
-      <button onClick={handleSignIn}>Sign in</button>
-      <button onClick={handleSignOut}>Sign out</button>
+  </div>
+  <label 
+      htmlFor="password" 
+      className="block test-sm font-medium test-gray-700"
+  >
+      Password
+  </label>
+  <div>
+      <input
+          type="password"
+          name="password"
+          id="password"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="password"
+          onChange = {(e) => setPassword(e.target.value)}
+      />
+  </div>
+  <button
+      type="submit"
+      className="flex items-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-10"
+  >
+      Sign in
+  </button>
+</form>
+
+<div className="flex flex-col justify-center items-center mt-10">
+<button 
+  onClick={handleSignInWithGoogle}
+  className="flex justify-center items-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-10"
+>
+      Sign in with Google
+</button>
+
+  <button 
+      onClick={handleSignUp}
+      className="flex justify-center items-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none mt-10"
+  >
+      Create an account
+  </button>
+
+<button 
+  onClick={handleSignOut}
+  className="flex justify-center items-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-10"
+>
+      Sign Out
+</button>
+</div>
     </>
   )
 }
