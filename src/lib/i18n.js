5a2e4926ee0d8b880ba6d9e7ce7ef672a7346bd8
nazimboudeffa <boudeffa@gmail.com>
2023-08-19 22:19:11 +0200

    Added job post

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,5 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+const options = {
+    order: [
+      "querystring",
+      "navigator",
+      "cookie",
+      "localStorage",
+      "sessionStorage",
+      "htmlTag",
+      "path",
+      "subdomain",
+    ],
+    lookupQuerystring: "lng",
+  };
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -19,7 +34,9 @@ const resources = {
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
+    detection: options,
     resources,
     lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
