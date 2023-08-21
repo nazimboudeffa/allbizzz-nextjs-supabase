401bccfd40d4ea2ec3636c9d51c6fe349d10f158
nazimboudeffa <boudeffa@gmail.com>
2023-08-21 20:54:38 +0200

    Added mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,12 @@ import Link from "next/link"
 import '@/lib/i18n';
 import { useTranslation } from 'react-i18next';
 import { ThemeToggle } from "@/components/theme-toggle"
+import { useState } from "react";
+import MobileMenu from "./MobileMenu";
 
 function Header () {
     const { t, i18n } = useTranslation();
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
         <header className="bg-white">
@@ -72,29 +75,34 @@ function Header () {
                 </div>
 
                 <div className="block md:hidden">
-                <button
-                    className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                >
+                <button type="button" className="p-2 lg:hidden" onClick={()=>setOpen(!open)}>
+                    { open ? 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>                  
+                    :
                     <svg
+                    className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    strokeWidth="2"
                     >
                     <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M4 6h16M4 12h16M4 18h16"
                     />
-                    </svg>
+                    </svg>                 
+                    }
                 </button>
                 </div>
                 <ThemeToggle />       
             </div>
             </div>
         </div>
+        <MobileMenu open={open} />
         </header>
     )
 }
