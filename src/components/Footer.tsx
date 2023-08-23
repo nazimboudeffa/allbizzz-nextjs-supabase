2cee332be1d87c84d78b4259801c15bebb540462
nazimboudeffa <boudeffa@gmail.com>
2023-08-23 23:15:47 +0200

    Added dark mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer () {
     return (
-        <footer className="bg-gray-50">
+        <footer>
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
             <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex justify-center text-gray-500 sm:justify-start">
