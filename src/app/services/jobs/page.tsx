e11f50a594102a793c45315958a95f0d83f85492
nazimboudeffa <boudeffa@gmail.com>
2023-08-23 18:17:06 +0200

    Fixing session

--- src/app/services/jobs/page.tsx
+++ src/app/services/jobs/page.tsx
@@ -45,7 +45,6 @@ const Jobs = function () {
 
     return (
         <>
-        <Header />
         <div className='w-full  py-20 flex items-center  justify-center flex-col'>
             <h1 className='text-xl mt-4 uppercase tracking-widest border-b-2 border-b-indigo-600 py-2 font-semibold mb-8 md:text-2xl lg:text-4xl'>Enter Job Details</h1>
             <form onSubmit={handleSubmit} className="sm:w-1/2 w-full px-4 mx-4  h-full" >
