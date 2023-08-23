eee353e325cc8baf2bd1f057b5791e64f67b516a
nazimboudeffa <boudeffa@gmail.com>
2023-08-23 16:09:40 +0200

    Crafted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ function Footer () {
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
             <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex justify-center text-gray-500 sm:justify-start">
-                <p>Made with <span className="text-red-500">❤</span> on <a href="https://github.com/nazimboudeffa/business-app-nextjs-supabase-tailwind">GitHub</a></p>
+                <p>Crafted with <span className="text-red-500">❤</span> on <a href="https://github.com/nazimboudeffa/business-app-nextjs-supabase-tailwind">GitHub</a></p>
             </div>
 
             <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
-                Copyright &copy; 2023. All rights reserved.
+                Copyright &copy; 2023. All rights reserved to the creators.
             </p>
             </div>
         </div>
