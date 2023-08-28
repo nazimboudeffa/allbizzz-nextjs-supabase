ad28f6ec5042ec693adfd7f0c4d15ab7737e024d
nazimboudeffa <boudeffa@gmail.com>
2023-08-28 19:57:35 +0200

    Adding icons to Ideas

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,6 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
 
 import { fontHeading } from "@/lib/fonts";
+import { Mail, Flag } from 'lucide-react';
 
 type Idea = {
   id: number;
@@ -44,7 +45,13 @@ function Welcome () {
     <section className="flex flex-col items-center gap-10 text-center">
         <div className="ml-5 mr-5 mt-10 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3">
         {ideas?.map((idea) => (
-          <div key={idea.id} className="p-5 shadow rounded-[12px] dark:shadow-slate-900">{idea.description}</div>
+          <div key={idea.id} className="flex flex-col p-5 shadow rounded-[12px] dark:shadow-slate-900">
+            <div className="text-xl mb-2">{idea.description}</div>
+            <div className="pl-5 pr-5 flex flex-row gap-2">
+            <Mail />
+            <Flag />
+            </div>
+          </div>
         ))}
         </div>
     </section>
