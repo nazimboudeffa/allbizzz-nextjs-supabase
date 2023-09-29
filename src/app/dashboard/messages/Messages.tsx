0858dd060c03224d5393894e7aad0090c6a23c36
nazimboudeffa <boudeffa@gmail.com>
2023-09-29 21:39:15 +0200

    Working on the messaging

--- src/app/dashboard/messages/Messages.tsx
+++ src/app/dashboard/messages/Messages.tsx
@@ -7,53 +7,65 @@ type Message = {
     receiver: string;
     sender: string;
     content: string;
+    createdAt: string;
   }
 
 function Messages() {
-
-    const [receivedMessages, setReceivedMessages] = useState<Message[] | null>([])
-    const [sentMessages, setSentMessages] = useState<Message[] | null>([])
+    const [allMessages, setAllMessages] = useState<Message[] | null>([])
 
     const supabase = createClientComponentClient();
+
     useEffect(() => {
-        const getReceivedMessages = async () => {
+        const getAllMessages = async () => {
             const { data: { user } } = await supabase.auth.getUser()
             const {
                 data: messages,
-            } = await supabase.from('messages').select().match({ receiver: user?.id });
-            setReceivedMessages(messages)
+            } = await supabase.from('messages').select().match({ receiver: user?.id } && { sender: user?.id });
+            setAllMessages(messages?.sort((a, b) => (a.createdAt > b.createdAt) ? 1 : -1) || [])
         }
-        const getSentMessages = async () => {
-            const { data: { user } } = await supabase.auth.getUser()
-            const {
-                data: messages,
-            } = await supabase.from('messages').select().match({ sender: user?.id });
-            setSentMessages(messages)
-        }
-        getSentMessages()
-        getReceivedMessages()
+        getAllMessages()
+        
     }, [supabase])
 
     return (
         <>
-        <div className="w-full md:w-[500px] items-center">
-            <h1 className="text-2xl font-bold mb-5">Your received messages</h1>
-            <div className="flex flex-col">
-            {receivedMessages && receivedMessages.map((message) => (
-                <div key={message.id} className="flex justify-between p-5 border border-input shadow rounded-[12px] dark:border-slate-900 dark:shadow-slate-900 mb-3">
-                    <div>{message.content}</div>
+        <div>This is not a chat, chat is not available at the moment and is a premium service</div>
+        <div className="h-full w-full flex flex-row shadow rounded-sm">
+            <div className="h-16">
+                <div className="flex items-center justify-between h-full px-6 border-b-2">
+                    <div className="flex items-center">
+                        <div className="text-xl font-semibold text-gray-700 dark:text-gray-200">Sender1</div>
+                    </div>
                 </div>
-            ))}
+                <div className="flex items-center justify-between h-full px-6 border-b-2">
+                    <div className="flex items-center">
+                        <div className="text-xl font-semibold text-gray-700 dark:text-gray-200">Sender2</div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between h-full px-6 border-b-2">
+                    <div className="flex items-center">
+                        <div className="text-xl font-semibold text-gray-700 dark:text-gray-200">Sender3</div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className="w-full md:w-[500px] items-center">
-            <h1 className="text-2xl font-bold mb-5">Your sent messages</h1>
-            <div className="flex flex-col">
-            {sentMessages && sentMessages.map((message) => (
-                <div key={message.id} className="flex justify-between p-5 border border-input shadow rounded-[12px] dark:border-slate-900 dark:shadow-slate-900 mb-3">
-                    <div>{message.content}</div>
+            <div className="flex flex-col justify-between w-full border-l-2">
+                <div className="flex flex-col">
+                    {allMessages && allMessages.map((message) => (
+                        <div key={message.id} className="flex justify-between p-5 border border-input shadow rounded-[12px] dark:border-slate-900 dark:shadow-slate-900 m-3">
+                            <div className="rounded bg-sky-600">Sender</div>
+                            <div>{message.content}</div>
+                        </div>
+                    ))}
                 </div>
-            ))}
+                <form className="bg-gray-100 flex items-center px-6 py-4 font-sans font-semibold">
+                    <input
+                        className="flex-grow mr-2 h-full border rounded placeholder-gray-500 font-semibold p-4 bg-gray-300 focus:bg-white focus:outline-none"
+                        placeholder="Type your message.." name="message" type="text" aria-label="Message input box"/>
+                    <button className="block w-32 h-full bg-blue-600 text-white py-2 px-4 font-semibold rounded"
+                            type="submit" style={{backgroundColor: "#1d6ed7"}}>
+                        Send
+                    </button>
+                </form>
             </div>
         </div>
         </>
--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -17,24 +17,10 @@ export default async function AllMessages() {
     return (
         <>
             <Header session = { session } />
-            <div className="grid min-h-screen md:grid-cols-[auto_1fr] justify-center gap-4 overflow-hidden p-4">
+            <div className="grid h-[calc(100vh-70px)] md:grid-cols-[auto_1fr] justify-center gap-4 overflow-hidden p-4">
             <SideNav />
-            <div className="min-h-screen w-full">
-              <header className="mt-10 flex flex-col items-center gap-10 text-center">
-              <div className="flex max-w-[980px] flex-col gap-2">
-                  <h1
-                      className={`text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl ${fontHeading.variable}`}
-                  >
-                      Your messages
-                  </h1>
-                  <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-                      Here you can find your messages.
-                  </p>
-              </div>
-              </header>
-              <section className="flex flex-col gap-10 mt-10 items-center">
+            <div className="h-full w-full">
                   {!session ? (<div>You must be loggedin</div>) : (<Messages />)}
-              </section>
             </div>
             </div>
 
